fc3930f565ea9389c02660de427eda4b2b5bcfed
Bartek Ciszkowski <bartek@wattpad.com>
2021-05-25 20:54:22 -0300

    Sync

--- nvim/lua/treesitter_setup.lua
+++ nvim/lua/treesitter_setup.lua
@@ -2,6 +2,7 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
+    additional_vim_regex_highlighting = true, -- improve highlighting, e.g. when spell is set
   },
 
   indent = {
@@ -19,9 +20,6 @@ require'nvim-treesitter.configs'.setup {
   },
   refactor = {
       highlight_definitions = {
-          enable = true
-      },
-      higlight_current_scope = {
           enable = true
       },
       smart_rename = {
