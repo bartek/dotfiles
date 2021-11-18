f31bb0476c85d5cfd0d9242de0ec28b8412a8e84
Bartek Ciszkowski <hello@justbartek.ca>
2021-11-17 20:23:32 -0400

    Remove antiquated .shell

--- .shell/scripts/irssinotify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- 
-(ssh admin@whahay.net -p1189 -o PermitLocalCommand=no \
-    ": > .irssi/fnotify ; cat .irssi/fnotify " | \
-    while read heading message; do \
-        terminal-notifier -title "${heading}" -message "${message}"; \
-    done)
