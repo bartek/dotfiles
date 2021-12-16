d862a095dc0fc9fa7b5c12109d20d17cfa89d7b6
Bartek Ciszkowski <hello@justbartek.ca>
2021-12-16 09:52:57 -0400

    Make nvim-cmp less frustrating

--- nvim/lua/cmp_setup.lua
+++ nvim/lua/cmp_setup.lua
@@ -1,6 +1,14 @@
 local cmp = require'cmp'
 
 cmp.setup({
+    completion = {
+        -- The minimum length of word to complete
+        keyword_length = 5,
+    },
+    experimental = {
+      ghost_text = true,
+      native_menu = false,
+    },
     snippet = {
         expand = function(args)
 
@@ -10,6 +18,8 @@ cmp.setup({
         end,
     },
     mapping = {
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
