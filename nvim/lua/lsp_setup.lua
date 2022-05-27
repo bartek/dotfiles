3bd41591a67d4487cc086c3066fb5b49bbfc8cf9
Bartek Ciszkowski <hello@justbartek.ca>
2022-05-27 11:47:58 -0300

    Fix goimports usage
    results was always nil. nvim/lsp api changes
    
    https://github.com/neovim/nvim-lspconfig/issues/115

--- nvim/lua/lsp_setup.lua
+++ nvim/lua/lsp_setup.lua
@@ -20,9 +20,32 @@ local buffer_setup = function(client)
     vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
--- Call goimports for Go files
-vim.api.nvim_command("au BufWritePre *.go lua goimports(500)")
+-- On save, call goimports for Go files. 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.go" },
+  callback = function()
+	  vim.lsp.buf.formatting_sync(nil, 3000)
+  end,
+})
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = { "*.go" },
+    callback = function()
+        local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+        params.context = {only = {"source.organizeImports"}}
+
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+        for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
+        end
+        end
+    end,
+})
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
