d22170e3b9129f191a970ac5868f1b5c7811ab62
Bartek Ciszkowski <hello@justbartek.ca>
2023-02-13 20:52:46 -0400

    Use my tree-sitter-vespa parser
    Have to specify it like so as it's not an official TS parser

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -66,3 +66,16 @@ require'nvim-treesitter.configs'.setup {
       }
   }
 }
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.vespa = {
+  install_info = {
+    url = "github.com/bartek/tree-sitter-vespa", -- local path or git repo
+    files = {"src/parser.c"},
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "sd", -- if filetype does not match the parser name
+}
