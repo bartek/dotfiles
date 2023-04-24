a1ec850fe1bb14278a8dcc96f225a5a5b66d3499
Bartek Ciszkowski <hello@justbartek.ca>
2023-04-24 13:09:51 -0300

    luasnip (needed for autocompletion) and some JS things

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -94,3 +94,4 @@ require'lspconfig'.zls.setup{}
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.rust_analyzer.setup{}
 require'lspconfig'.ocamlls.setup{}
+require'lspconfig'.tsserver.setup{}
