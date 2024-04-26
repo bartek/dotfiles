eb44e52ee46d88ed766ccd0f0eb62a1609f9f4ce
Bartek Ciszkowski <hello@justbartek.ca>
2024-04-26 11:38:15 -0300

    Add yaml to tree sitter parsers

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -11,6 +11,7 @@ require'nvim-treesitter.configs'.setup {
     "python",
     "svelte",
     "typescript", 
+    "yaml",
     "zig",
   },
 
@@ -22,9 +23,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
-
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
