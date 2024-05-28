b2a3bb59ce2ffb4d63f9548c62e7e3ace076bf46
Bartek Ciszkowski <hello@justbartek.ca>
2024-05-28 10:06:52 -0300

    Sync and fix issue with go plugin after 0.10

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -19,25 +19,39 @@ local buffer_setup = function(client)
     vim.api.nvim_buf_set_option(0, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.go", "*.zig" },
-    callback = function()
-        vim.lsp.buf.format({async = true})
-        local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
-        params.context = {only = {"source.organizeImports"}}
-
-        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-        for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-            if r.edit then
-                vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
-            else
-                vim.lsp.buf.execute_command(r.command)
-            end
-        end
-        end
-    end,
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  pattern = '*.go',
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format({async = false, id = args.data.client_id})
+      end,
+    })
+  end
 })
+
+-- Import packages automatically on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = vim.api.nvim_create_augroup('setGoFormatting', { clear = true }),
+  pattern = '*.go',
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 2000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end
+})
+
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
