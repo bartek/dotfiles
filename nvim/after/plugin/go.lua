0f746fb6e10616040736aaa4d8bd75722468df4c
Bartek Ciszkowski <hello@justbartek.ca>
2024-05-28 10:06:52 -0300

    Sync and fix issue with go plugin after 0.10

--- nvim/after/plugin/go.lua
+++ nvim/after/plugin/go.lua
@@ -1,4 +1,10 @@
-require('go').setup()
+require('go').setup({
+    -- Disable LSP stuff as already configured elsewhere
+    lsp_codelens = false,
+    lsp_inlay_hints = {
+        enable = false,
+    },
+})
 
 vim.keymap.set('n', '<leader>t', '<cmd>GoTest -f<cr>', {})
 vim.keymap.set('n', '<leader>tt', '<cmd>GoTest<cr>', {})
