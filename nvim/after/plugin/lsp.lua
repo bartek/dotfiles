3baaff6e333014a200941d30b45820a0ba63f108
Bartek Ciszkowski <hello@justbartek.ca>
2025-04-24 21:58:28 -0300

    Use mini, fix deprecated

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -37,7 +37,7 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   group = vim.api.nvim_create_augroup('setGoFormatting', { clear = true }),
   pattern = '*.go',
   callback = function()
-    local params = vim.lsp.util.make_range_params()
+    local params = vim.lsp.util.make_range_params(0, "utf-8")
     params.context = { only = { "source.organizeImports" } }
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 2000)
     for _, res in pairs(result or {}) do
